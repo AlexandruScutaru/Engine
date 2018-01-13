8c86c902bd5184d5f7c5c0ed21b938d6cc93b0ce
AlexandruScutaru <alex_cw15@yahoo.com>
2018-01-13 03:10:46 +0200

    ImGui integration.

--- WorldEditor3D/MainApp.cpp
+++ WorldEditor3D/MainApp.cpp
@@ -1,16 +1,21 @@
 #include "MainApp.h"
 
-#define SCREEN_WIDTH 800	
-#define SCREEN_HEIGHT 600
+#include <iostream>
+
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 576
 
 
 MainApp::MainApp():
-	m_appState(AppState::EDIT)
+	m_appState(AppState::EDIT),
+	m_collapseEditor(false)
 {
 	initSystems();
 }
 
-MainApp::~MainApp(){}
+MainApp::~MainApp(){
+	ImGui_ImplSdlGL3_Shutdown();
+}
 
 
 void MainApp::run(){
@@ -20,10 +25,14 @@ void MainApp::run(){
 
 void MainApp::initSystems(){
 	m_window.create("Renderer", SCREEN_WIDTH, SCREEN_HEIGHT/*, renderer::WindowFlags::BORDERLESS*/);
+	
+	//Setup ImGui
+	ImGui_ImplSdlGL3_Init(m_window.getWindow());
+	ImGui::StyleColorsDark();
 }
 
 void MainApp::initLevel(){
-	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 }
 
 void MainApp::loop(){
@@ -37,6 +46,7 @@ void MainApp::loop(){
 void MainApp::processInput(){
 	SDL_Event e;
 	while(SDL_PollEvent(&e)) {
+		ImGui_ImplSdlGL3_ProcessEvent(&e);
 		switch(e.type) {
 		case SDL_QUIT:
 			// Exit the app here!
@@ -47,12 +57,32 @@ void MainApp::processInput(){
 }
 
 void MainApp::update(float deltaTime){
+	updateImGuiWindows();
+}
 
+void MainApp::updateImGuiWindows(){
+	ImGui_ImplSdlGL3_NewFrame(m_window.getWindow());
+
+	showEditorWindow();
 }
 
 void MainApp::drawGame(){
+	glViewport(0, 0, (int)ImGui::GetIO().DisplaySize.x, (int)ImGui::GetIO().DisplaySize.y);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+	ImGui::Render();
 	//sdl: swap buffers
 	m_window.swapBuffer();
+}
+
+/// ImGui
+void MainApp::showEditorWindow(){
+	ImGui::SetNextWindowCollapsed(false, ImGuiCond_Once);
+	ImGui::SetNextWindowPos(ImVec2(SCREEN_WIDTH-300.0f, 0.0f), ImGuiSetCond_Once);
+	ImGui::SetNextWindowSize(ImVec2(300, SCREEN_HEIGHT), ImGuiCond_Once);
+
+	ImGui::Begin("editor", NULL);
+	
+	ImGui::Text("test");
+	
+	ImGui::End();
 }
--- WorldEditor3D/MainApp.h
+++ WorldEditor3D/MainApp.h
@@ -1,6 +1,8 @@
 #ifndef MAINAPP_H
 #define MAINAPP_H
 
+#include <ImGUI/imgui.h>
+#include "imgui_impl_sdl_gl3.h"
 #include <Renderer/Renderer.h>
 
 
@@ -23,11 +25,16 @@ private:
 	void loop(); //main app loop
 	void processInput(); //handles input processing
 	void update(float deltaTime); //updates based on deltatime
+	void updateImGuiWindows();
 	void drawGame(); //draws on screen
 
 	renderer::Window m_window; //the app window
 
 	AppState m_appState;
+
+	//imgui variables and methods
+	void showEditorWindow();
+	bool m_collapseEditor;
 };
 
 #endif // !MAINAPP_H
